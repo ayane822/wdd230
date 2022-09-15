769d91964da8b63d7f3bf4ab2b377b4e5503ceb0
KurokawaAyane <ayane@KurokawaAyanes-MacBook-Air.local>
2022-09-15 11:17:03 -0600

    update

--- practice.js
+++ practice.js
@@ -1,3 +1,16 @@
 //sequence
-let date = new date();
-console.log(date)
+let date = new Date();
+
+console.log(date);
+
+let year = date.getFullYear();
+
+console.log(year);
+
+document.querySelector('h1').textContent = year ;
+
+let currentdate = document.lastModified;
+
+console.log(currentdate);
+
+document.querySelector('div').textContent = currentdatef
