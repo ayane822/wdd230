7ff5ac2b9fed66d2075bff599ba2f297f92d0542
Aya Kurokawa <kur20002@byui.edu>
2022-10-09 16:04:10 -0600

    W4 update

--- chamber/chamber.js
+++ chamber/chamber.js
@@ -1,5 +1,11 @@
-const updateDate = document.getElementById('last-update');
-updateDate.innerHTML = "Last Updated: " + document.lastModified;
 
-const today =new Date();
-console.log(today);
+const updateDate = document.getElementById('last-update');
+updateDate.innerHTML = "Last Modification: " + document.lastModified;
+
+
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const Tday = new Date();
+let day = days[Tday.getDay()];
+let month = months[Tday.getMonth()];
+document.getElementById('today').innerHTML = Tday.toDateString();
