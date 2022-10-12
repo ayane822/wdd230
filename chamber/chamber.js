d12be0b2055fb2a164161b94ab3be96fcad7ef31
Aya Kurokawa <kur20002@byui.edu>
2022-10-12 11:43:26 -0600

    DOM hw

--- chamber/chamber.js
+++ chamber/chamber.js
@@ -3,8 +3,8 @@ const updateDate = document.getElementById('last-update');
 updateDate.innerHTML = "Last Modification: " + document.lastModified;
 
 
-const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+// const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 const Tday = new Date();
 let day = days[Tday.getDay()];
 let month = months[Tday.getMonth()];
