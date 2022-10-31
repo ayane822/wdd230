695ed34c44e5f2f7a3edb613d119be4f74759646
Aya Kurokawa <kur20002@byui.edu>
2022-10-31 10:53:11 -0600

    modify

--- chamber2/discover.js
+++ chamber2/discover.js
@@ -30,5 +30,43 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 // footer
-const updateDate = document.getElementById('last-update');
-updateDate.innerHTML = "Last Modification: " + document.lastModified;
+// const updateDate = document.getElementById('last-update');
+// updateDate.innerHTML = "Last Modification: " + document.lastModified;
+
+
+// let documentItem = document.getElementById('discover_page_date')
+
+// if (localStorage.getItem('lastVisit')){
+//   let previousItem = localStorage.getItem('lastVisit')
+//   let thisDate = Date.now();
+//   let subtractValue = thisDate - previousItem;
+//   let editSubtract = subtractValue/1000/60/60/24;
+//   document.innerHTML = Math.round(editSubtract);
+// }
+// else {
+//   localStorage.setItem('lastVisit',Date.now());
+//   let todaysDate = localStorage.getItem('lastVisit');
+//   documentItem.innerHTML = todaysDate;
+// }
+
+let documentItem = document.getElementById('discover_page_date')
+
+if(localStorage.getItem('lastVisit')) {
+    // Get value from Local Storage
+    let previousItem = localStorage.getItem('lastVisit')
+    // Get Todays Date
+    let thisDate = Date.now();
+    // Subtract the variables
+    let subtractValue = thisDate - previousItem;
+    let editSubtract = subtractValue/1000/60/60/24;
+    documentItem.innerHTML = Math.round(editSubtract);
+
+
+}
+
+else {
+    localStorage.setItem('lastVisit',Date.now());
+    let todaysDate = localStorage.getItem('lastVisit');
+    documentItem.innerHTML = todaysDate;
+
+}
