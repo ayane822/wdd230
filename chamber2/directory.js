a30beebd8d0ebca8cc7cbbbe060331ad53ce53b2
Aya Kurokawa <kur20002@byui.edu>
2022-11-13 00:20:09 -0700

    pic

--- chamber2/directory.js
+++ chamber2/directory.js
@@ -1,1 +1,20 @@
-const requestURL = 'directory.json'
+const requestURL = 'https://ayane822.github.io/wdd230/businessjson/directory.json'
+
+async function getBusiness(requestURL){
+    const response = await fetch(requestURL);
+    console.log(response);
+    if (response.ok){
+        const jsobject = await response.json();
+        console.log(jsobject);
+        const business = jsobject['businesses']
+        console.log(businesses[0].lastname)
+        businesses.forEach(displayBusiness);
+    }
+}
+
+getBusiness(requestURL);
+
+function displayBusiness(item){
+    let card = document.createElement('section');
+    let 
+}
