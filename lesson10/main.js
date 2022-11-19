573b5fd042fb1a0f2e00a6c00fa42281189b33a9
Aya Kurokawa <kur20002@byui.edu>
2022-11-19 14:41:29 -0700

    weather api

--- lesson10/main.js
+++ lesson10/main.js
@@ -1,21 +1,3 @@
-
-const updateDate = document.getElementById('last-update');
-updateDate.innerHTML = "Last Modification: " + document.lastModified;
-
-
-// const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-// const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-// const Tday = new Date();
-// let day = days[Tday.getDay()];
-// let month = months[Tday.getMonth()];
-// document.getElementById('today').innerHTML = Tday.toDateString();
-
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
-const d = new Date();
-let day = weekday[d.getDay()];
-document.getElementById("today").innerHTML = day;
-
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=1858296&appid=17512653d8d6020390bf626d84d1e193';
 
 fetch(apiURL)
